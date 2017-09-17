5b386e3f600558aa8e4bdb3ae64dc176190de4fd
Ian <ianmilliken@live.co.uk>
2017-09-17 15:02:58 +1000

    [#4] Modules are working

--- src/js/site/siteDrawer.js
+++ src/js/site/siteDrawer.js
@@ -1,21 +1,19 @@
 export default function SiteDrawer() {
 
-  console.log('----------------------------------------');
   console.log('SiteDrawer has loaded');
   console.log('----------------------------------------');
 
-  const hamburger = $('.site-nav__hamburger');
-  const overlay = $('.site-overlay');
-  const drawer = $('.site-drawer');
-  const close = $('.site-drawer__close');
+  const hamburger = $('.site-nav__hamburger'),
+        overlay = $('.site-overlay'),
+        drawer = $('.site-drawer'),
+        close = $('.site-drawer__close'),
 
-  const add_classes = function() {
-    console.log('adding classes');
+  add_classes = function() {
     overlay.addClass( 'site-overlay--active' );
     drawer.addClass( 'site-drawer--active' );
-  };
+  },
 
-  const remove_classes = function() {
+  remove_classes = function() {
     console.log('removing classes');
     overlay.removeClass( 'site-overlay--active' );
     drawer.removeClass( 'site-drawer--active' );
@@ -23,14 +21,12 @@ export default function SiteDrawer() {
 
   hamburger.off();
   hamburger.on('click', function(event) {
-    event.preventDefault();
     console.log('BOOM!');
     add_classes();
   });
 
   close.off();
   close.on('click', function(event) {
-    event.preventDefault();
     remove_classes();
   });
 
